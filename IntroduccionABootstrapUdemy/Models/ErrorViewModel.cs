3bf5084e4e62b1d0adce97468ebee20e22af7b0a
Omar Sanchez <ing_omarglez@hotmail.com>
2023-09-29 15:26:09 -0600

    Creando mi portafolio en .NET, Blazor, C#

---
+++ IntroduccionABootstrapUdemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IntroduccionABootstrapUdemy.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
